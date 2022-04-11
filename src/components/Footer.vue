2d93e0ffc46b5863a32634157e4af27cc4906f2e
Yenifer Ramírez <yeniferramirez11@gmail.com>
2022-04-11 08:38:24 -0400

    Creación de componentes y página de inicio

---
+++ src/components/Footer.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-footer dark padless>
+    <v-card flat tile class="black white--text py-12 px-5" width="100%">
+      <v-row>
+        <v-col cols="12" xs="12" sm="6" md="4" class="pa-16">
+          <v-toolbar color="transparent" flat>
+            <v-badge color="#d5f0db" dot>
+              <v-img src="1.png" contain width="30"></v-img>
+            </v-badge>
+            <v-toolbar-title class="ml-4">
+              <span class="green--text">e</span><strong>Grocery</strong>
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text class="pt-0">
+            <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-1 white--text"
+              icon
+              fab
+              outlined
+              small
+            >
+              <v-icon size="20px">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>About</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> How it Work</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Our Promrams</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Promotions</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Refer a Friend</v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>Delivery</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> Cost on Delivery</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Payment Method</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Delivery Status</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Returns</v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>Contact</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> Yenifer Rosana</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Mérida, Venezuela</v-card-text>
+          <v-card-text class="grey--text mt-n4"> @yeniferrosana</v-card-text>
+          <v-card-text class="grey--text mt-n4"> B81234567</v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-footer>
+</template>
+<script>
+export default {
+  data: () => ({
+    icons: [
+      "fab fa-apple",
+      "fab fa-google-plus-g",
+      "fab fa-twitter",
+      "fab fa-facebook-f"
+    ]
+  }),
+}
+</script>
---
+++ src/components/Nabvar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar app color="white" flat>
+    <v-badge color="#D5F0DB" dot>
+      <v-img src="1.png" contain width="30"></v-img>
+    </v-badge>
+    <v-toolbar-title class="ml-4">
+      <span class="green--text">e</span><strong>Grocery</strong>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-badge color="#ff6d59" overlap content="2" class="mr-2 mt-1">
+      <v-avatar color="fff0ee" size="40">
+        <v-icon x-small color="#ff6d59">far fa-heart</v-icon>
+      </v-avatar>
+    </v-badge>
+    <v-badge color="#41ab55" overlap content="3" class="mr-2 mt-1">
+      <v-avatar color="#ECF7EE" size="40">
+        <v-icon x-small color="#41ab55">fas fa-shopping-cart</v-icon>
+      </v-avatar>
+    </v-badge>
+    <span class="grey--text d-none d-sm-flex">Hola, <strong> Rosana</strong></span>
+    <v-avatar size="30" class="ml-2">
+      <v-img src="avatar.jpg"></v-img>
+    </v-avatar>
+    <v-btn icon dark>
+      <v-icon x-small color="#878a94">fas fa-chevron-down</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+<style scoped>
+.v-toolbar__title{
+  font-size: 1rem !important;
+}
+</style>
